e3a2258102d8cb5e4d27537254817f9c48bb2272
Jiawei Chen <cjwcommuny@outlook.com>
2026-03-15 22:43:43 +0800

    init

--- CapslockNoDelay/CapslockNoDelayApp.swift
+++ CapslockNoDelay/CapslockNoDelayApp.swift
@@ -6,12 +6,40 @@
 //
 
 import SwiftUI
+import os
+
+let logger = Logger()
 
 @main
 struct CapslockNoDelayApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
+    }
+
+    init() {
+        Task {
+            await run()
+            await MainActor.run {
+                NSApp.terminate(nil)
+            }
+        }
+    }
+}
+
+func run() async {
+    let process = Process()
+    process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+    process.arguments = [
+        "hidutil", "property", "--set", "{\"CapsLockDelayOverride\":0}",
+    ]
+    do {
+        try process.run()
+    } catch {
+        logger
+            .error(
+                "ERROR: failed to execute capslock no-delay shell commands, error: \(error.localizedDescription)"
+            )
     }
 }
