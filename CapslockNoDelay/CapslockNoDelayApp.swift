aa7a91365541505ae2b14d8fa660e9c479ecff0c
Jiawei Chen <cjwcommuny@outlook.com>
2026-03-15 22:23:10 +0800

    Initial Commit

---
+++ CapslockNoDelay/CapslockNoDelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapslockNoDelayApp.swift
+//  CapslockNoDelay
+//
+//  Created by Jiawei Chen on 2026/3/15.
+//
+
+import SwiftUI
+
+@main
+struct CapslockNoDelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
